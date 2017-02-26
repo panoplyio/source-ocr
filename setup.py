3e263782843308f7e34cfac2cc35c55c1a21848b
Lior Rozen <lixroz+github@gmail.com>
2017-02-26 11:43:44 +0200

    Add install requirements and test travis build

--- setup.py
+++ setup.py
@@ -8,7 +8,8 @@ setup(
     author_email='lior@panoply.io',
     url='http://panoply.io',
     install_requires=[
-        'panoply-python-sdk==1.3.2'
+        'panoply-python-sdk==1.3.2',
+        'mock==2.0.0',
     ],
 
     # place this package within the panoply package namespace
